390007c50e6f163ff7596f16469615fc81d004c7
sidali zitouni terki <sidalizitouniterki@MacBook-Air-de-sidali.local>
2024-04-29 18:23:37 +0200

    implementation du pierre feuille ciseau en C

---
+++ pierre_feuille_ciseaux/C/main.c
@@ -0,0 +1,12 @@
+#include "pfc.h"
+
+
+int main(int argc, const char * argv[]) {
+    
+    
+    pfc(3);
+    
+    
+    
+    return 0;
+}
---
+++ pierre_feuille_ciseaux/C/pfc.h
@@ -0,0 +1,25 @@
+#ifndef pfc_h
+#define pfc_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<ctype.h>
+#include <string.h>
+#include <time.h>
+
+/* ordinateur joue aléatoirement*/
+int randomPlayer();
+
+/* jeu de l'humain*/
+int humanPlayer();
+
+/* on choisit si c'est un humain ou l'ordinateur */
+char choixjoueur(char nomplayer[10]);
+
+/* choisir le joueur soit humain ou random*/
+int choix_joueur(char);
+
+/* Pierre Feuille Ciseau*/
+void pfc(int);
+
+#endif /* pfc */
